cae37b3cb0267122344057a634994ece553168d7
ConnorIllingworth <46832931+ConnorIllingworth@users.noreply.github.com>
2025-07-07 08:49:07 -0700

    MarkDoc Checkin

--- next.config.ts
+++ next.config.ts
@@ -4,4 +4,9 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
+// const withMarkdoc = require('@markdoc/next.js');
+// const nextConfig: NextConfig = withMarkdoc(/* options */)({
+//   pageExtensions: ['md', 'mdoc', 'js', 'jsx', 'ts', 'tsx']
+// });
+
 export default nextConfig;
